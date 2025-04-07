f92bb98d942defaa7b862d62dd7a55bfd510b040
Fedmichard <francoisfedmichard@gmail.com>
2025-04-06 20:31:09 -0400

    Setup frontend and backend, will learning react + asp.net webapi separately, will combine once I achieve about intermediate knowledge

---
+++ server/Program.cs
@@ -0,0 +1,24 @@
+// Controls things like dependancy injection
+// Provide with services and things to add to program
+// Almost like module
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Your app, controls your actual http request pipeline
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// Middleware for request and responses
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
