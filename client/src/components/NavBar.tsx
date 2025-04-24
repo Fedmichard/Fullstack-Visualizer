40217bcdb7fa7e4b33f18ddc83c07d5fa2ff06fc
Fedmichard <francoisfedmichard@gmail.com>
2025-04-23 20:31:10 -0400

    Slow and Steady Wins the race, will continue to relearn basic react skills then start the layout of the site!

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div>
+                <h1>NavBar!</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
