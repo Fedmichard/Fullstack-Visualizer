52670216e2e7d07774df87844ef042c9b4be1d96
Fedmichard <francoisfedmichard@gmail.com>
2025-04-25 21:02:28 -0400

    Will continue learning somewhere else

--- server/Program.cs
+++ server/Program.cs
@@ -1,18 +1,13 @@
-// Controls things like dependancy injection
-// Provide with services and things to add to program
-// Almost like module
-var builder = WebApplication.CreateBuilder(args); // Var tells compiler to figure out the variable type (like auto in c#)
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Your app, controls your actual http request pipeline
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// Middleware for request and responses
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,6 +16,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-/* DEFINE API END POINTS HERE */
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
 
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29afc49de2e24b2fc0c540f11196a1c04ba67d85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eaabbe318830ddae92b1a8f89e7df2aad566c1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
