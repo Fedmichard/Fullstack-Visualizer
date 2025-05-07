5d86066261b874a3b23dfdb053152b1b490b3e64
Fedmichard <francoisfedmichard@gmail.com>
2025-05-07 16:56:39 -0400

    Added some progress to my about and home page, still need to figure out how I'll lay the website out and colors!

--- client/src/pages/about/index.tsx
+++ client/src/pages/about/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import './style.css';
+
 const About = () => {
     return (
-        <main>
-            <section>
-                <h1>Fedmichard Francois</h1>
+        <main className='about-container'>
+            <section className='about-me'>
+                <h1>About Me!</h1>
+                <a>My Name is Fedmichard Francois!</a>
+            </section>
+            <section className='about-more'>
+                <h1>Another Ting!</h1>
             </section>
         </main>
     )
--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -3,9 +3,15 @@ import './style.css'
 
 const Home = () => {
     return (
-        <main>
-            <h1>Home Page!</h1>
-            <h2><a href='/Application'>Click here to test my app!</a></h2>
+        <main className='home-container'>
+            <section className='app-info'>
+                <h1>Home Page!</h1>
+                <h2><a href='/Application'>Click here to test my app!</a></h2>
+            </section>
+            <section className='more-info'>
+                <h1>Next Stop!</h1>
+                <h2>And another thing!</h2>
+            </section>
         </main>
     )
 }
