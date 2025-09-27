ba4204e95069a8a1e0029a6ddf0f64a1337e40dd
Fedmichard <francoisfedmichard@gmail.com>
2025-09-27 18:24:03 -0400

    checking sum

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+433fb6d5c8705be0bf5060f0ca0a7103ea0a8036")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e7eba7bec1f0f441cd3b573ff78edd473e9dd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
